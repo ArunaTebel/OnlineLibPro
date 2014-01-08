886538fb3eba219c6de91c7240dbd21aa2778a23
ArunaTebel <arunatebel@hotmail.com>
2014-01-08 13:23:42 +0530

    New Commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="CSS/index.css">
+        <title></title>
+    </head>
+    <body>
+
+        <div id="top_section">
+            <img src="Images/lgo.png" height="100px" style="margin-left: 10px; margin-top: 10px">
+            <div id="menu">
+                <div class="menuItem">
+                    HOME
+                </div>
+                <div class="menuItem">
+                    DOWNLOADS
+                </div>
+                <div class="menuItem">
+                    HELP
+                </div>
+                <div class="menuItem">
+                    CONTACT US
+                </div>
+            </div>
+            <div id="signIn">                
+                <input type="text" id="username" class="input" placeholder="Username"><input type="password" id="password" class="input" placeholder="Username"><input type="submit" class="button" value="SUBMIT">                
+            </div>
+        </div>
+        <div id="middle_section">
+            <div id="one">
+                <a href="http://www.google.com"><img src="Images/about.png" height="75px" class="menuTitle"></a>
+            </div>
+            <div id="two">
+                <img src="Images/news.png" height="32px"  class="menuTitle">
+            </div>
+            <div id="three">
+                <img src="Images/signup.png" height="45px"  class="menuTitle">
+            </div>
+            <div id="four" >
+                <img src="Images/downloads.png" height="25px" class="menuTitle">
+            </div>
+            <div id="five">
+                <img src="Images/catalogue.png" height="15px" class="menuTitle">
+            </div>
+        </div>
+
+        <div id="bottom_section">
+            
+        </div>
+    </body>
+</html>
+
+<?php ?>
